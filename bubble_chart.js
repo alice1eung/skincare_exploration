da1547c44e00d3104f85623c92cce96ece88964b
Alice Leung <alice1eung2504@gmail.com>
2018-09-22 21:27:20 -0400

    added comments to bubble_chart.js

--- bubble_chart.js
+++ bubble_chart.js
@@ -1,3 +1,7 @@
+
+//starting off with code from the bloew link and adapting it to make it more interactive
+//https://medium.freecodecamp.org/a-gentle-introduction-to-d3-how-to-build-a-reusable-bubble-chart-9106dc4f6c46
+
 function bubbleChart() {
     var width = 960,
         height = 960,
